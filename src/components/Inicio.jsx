bdcefaa81809eb7563b660ebb26ad2927bc468a7
Franco Brumatti <franco.brumatti@gmail.com>
2022-01-18 20:00:42 -0300

    redes sociales de la navbar

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
+
+import Navbar from './Navbar';
 const Inicio = () => {
   const ImagenFondo = styled.img`
     width: 100%;
@@ -7,6 +9,7 @@ const Inicio = () => {
   `;
   return (
     <div>
+      <Navbar />
       <ImagenFondo src="media/inicioFondo.jpeg" alt="panaderia fondo" />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,82 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { SocialIcon } from 'react-social-icons';
 
 const Navbar = () => {
 
     const Nav = styled.nav`
         z-index: 1;
         display: flex;
-        justify-content: space-around;
-        background-color: rgba(255, 255, 255, 0);
+        justify-content: space-between;
+        align-items: center;
+        min-height: 6rem;
+        background-color: transparent;
+    `;
+
+    const Botones = styled.div`
+        display: flex;
+        text-decoration: none;
+        gap: 2rem;
+        margin-right: 2rem;
+        background-color: transparent;
+    `;
+
+    const Logo = styled.a`
+        text-decoration: none;
+        margin-left: 5rem;
+    `;
+
+    const Redes = styled.div`
+        display: flex;
+        flex-direction: column;
+    `;
+
+    const RedesFranco = styled.div`
+        display: flex;
+        justify-content: space-between;
+        background-color: rgba(0, 0, 0, 0.2);
+        padding: .5rem;
+        gap: .5rem;
+        align-items: center;
+    `;
+
+    const RedesTomi = styled.div`
+        display: flex;
+        justify-content: space-between;
+        background-color: rgba(0, 0, 0, 0.15);
+        padding: .5rem;
+        gap: .5rem;
+        align-items: center;
     `;
 
     return ( 
         <Nav>
-            <div>
+            <Logo>
                 Logo
-            </div>
-            <div>
-                <h1>boton</h1>
-                <h1>boton</h1>
-                <h1>boton</h1>
-                <h1>boton</h1>
+            </Logo>
+            <div className='d-flex align-items-center'>
+                <Botones>
+                    <a>boton</a>
+                    <a>boton</a>
+                    <a>boton</a>
+                    <a>boton</a>
+                </Botones>
+                <Redes>
+                    <RedesFranco>
+                        <h3>Franco Brumatti</h3>
+                        <div className='justify-content-between'>
+                            <SocialIcon className='mx-2' url="https://github.com/FrancoBrumatti" network="github" />
+                            <SocialIcon url="https://www.linkedin.com/in/franco-brumatti/" network="linkedin" />
+                        </div>
+                    </RedesFranco>
+                    <RedesTomi>
+                        <h3>Tomas Ghilino</h3>
+                        <div className='justify-content-between'>
+                            <SocialIcon className='mx-2' url="https://github.com/FrancoBrumatti" network="github" />
+                            <SocialIcon url="https://www.linkedin.com/in/franco-brumatti/" network="linkedin" />
+                        </div>
+                    </RedesTomi>
+                </Redes>
             </div>
         </Nav>
     );

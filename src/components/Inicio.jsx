65add4de2e6f0ead5af86d6e87f60018fcbda6bb
Tomas Ghilino <tomasghilino01@gmail.com>
2022-01-18 20:06:50 -0300

    primeros pasos inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import SwiperCore, { Autoplay, Pagination, Navigation } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Autoplay, Pagination, Navigation]);
+
 const Inicio = () => {
-  const ImagenFondo = styled.img`
+  const Img = styled.img`
     width: 100%;
     height: 100vh;
   `;
+
+  const Hh1 = styled.h1`
+    position: fixed;
+    top: 0;
+    z-index: 5;
+  `;
   return (
     <div>
-      <ImagenFondo src="media/inicioFondo.jpeg" alt="panaderia fondo" />
+      <Hh1>ESTO ES UNA NAVBARBRO</Hh1>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={false}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Img src="media/inicioFondo1.jpeg" alt="fondo" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Img src="media/inicioFondo2.jpeg" alt="fondo" />
+        </SwiperSlide>
+      </Swiper>
+      <Img src="media/inicioFondo2.jpeg" alt="fondo" />
+      <Img src="media/inicioFondo2.jpeg" alt="fondo" />
     </div>
   );
 };

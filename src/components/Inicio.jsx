6865e94ff050a926b85a9b4d6625e2a446cfb262
Tomas Ghilino <tomasghilino01@gmail.com>
2022-01-19 18:11:07 -0300

    fix

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,54 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-import SwiperCore, { Autoplay, Pagination, Navigation } from 'swiper';
-
-// install Swiper modules
-SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 const Inicio = () => {
-  const Img = styled.img`
-    width: 100%;
+  const Div = styled.div`
+    background-image: url('media/inicioFondo1.jpeg');
     height: 100vh;
   `;
-
-  const Hh1 = styled.h1`
-    position: fixed;
-    top: 0;
-    z-index: 5;
-  `;
   return (
-    <div>
-      <Hh1>ESTO ES UNA NAVBARBRO</Hh1>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={false}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <Img src="media/inicioFondo1.jpeg" alt="fondo" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Img src="media/inicioFondo2.jpeg" alt="fondo" />
-        </SwiperSlide>
-      </Swiper>
-      <Img src="media/inicioFondo2.jpeg" alt="fondo" />
-      <Img src="media/inicioFondo2.jpeg" alt="fondo" />
-    </div>
+    <Div>
+      <h1>hola</h1>
+    </Div>
   );
 };
 

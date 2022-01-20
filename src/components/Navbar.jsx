b544aaa433f88b01d49b58eb6ee568324c688630
Franco Brumatti <franco.brumatti@gmail.com>
2022-01-20 11:52:54 -0300

    logica y ajustes de burguermenu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from '@emotion/styled';
-import { SocialIcon } from 'react-social-icons';
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
 
     const Nav = styled.nav`
         z-index: 1;
@@ -11,6 +12,7 @@ const Navbar = () => {
         align-items: center;
         min-height: 6rem;
         background-color: rgba(0, 0, 0, 0.4);
+        border-bottom: 2px solid #00000078;
 
         /* sombra */
         box-shadow: -2px -3px 17px 1px rgba(0,0,0,0.75);
@@ -52,8 +54,26 @@ const Navbar = () => {
         }
     `;
 
-    const BurguerMenu = styled.button`
-       
+    const BurguerIcon = styled.button`
+       border: none;
+       background-color: transparent;
+       transition: all 0.3s;
+
+       &:hover{
+           filter: brightness(120%);
+       }
+
+       @media (min-width: 1024px) {
+            display: none;
+        }
+    `;
+
+    const BurguerMenu = styled.div`
+        display: flex;
+        flex-direction: column;
+        position: relative;
+        top: 20px;
+        z-index: 2;
     `;
 
     // const Redes = styled.div`
@@ -87,17 +107,51 @@ const Navbar = () => {
             <Logo>
                 <img className='w-100 h-100' src='media/logo.png' alt="" />
             </Logo>
-            <Botones>
-                <Boton>Inicio</Boton>
-                <Boton>Nosotros</Boton>
-                <Boton>Catering</Boton>
-                <Boton>Contacto</Boton>
-            </Botones>
-            <BurguerMenu>
-                <div>
-
-                </div>
-            </BurguerMenu>
+            <div className='d-flex'>
+                <Botones>
+                    <Boton>Inicio</Boton>
+                    <Boton>Nosotros</Boton>
+                    <Boton>Catering</Boton>
+                    <Boton>Contacto</Boton>
+                </Botones>
+                <BurguerIcon
+                    onClick={() => setOpen(!open)}
+                    open={open}
+                >
+                    <div className='me-4'>
+                        <svg
+                            width="44"
+                            height="44"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M2 6C2 5.44772 2.44772 5 3 5H21C21.5523 5 22 5.44772 22 6C22 6.55228 21.5523 7 21 7H3C2.44772 7 2 6.55228 2 6Z"
+                                fill="#b1002c"
+                            />
+                            <path
+                                d="M2 12.0322C2 11.4799 2.44772 11.0322 3 11.0322H21C21.5523 11.0322 22 11.4799 22 12.0322C22 12.5845 21.5523 13.0322 21 13.0322H3C2.44772 13.0322 2 12.5845 2 12.0322Z"
+                                fill="#b1002c"
+                            />
+                            <path
+                                d="M3 17.0645C2.44772 17.0645 2 17.5122 2 18.0645C2 18.6167 2.44772 19.0645 3 19.0645H21C21.5523 19.0645 22 18.6167 22 18.0645C22 17.5122 21.5523 17.0645 21 17.0645H3Z"
+                                fill="#b1002c"
+                            />
+                        </svg>
+                        {open ?
+                        <BurguerMenu>
+                            
+                                <Boton>Inicio</Boton>
+                                <Boton>Nosotros</Boton>
+                                <Boton>Catering</Boton>
+                                <Boton>Contacto</Boton>
+                            
+                        </BurguerMenu>
+                        : null}
+                    </div>
+                </BurguerIcon>
+            </div>
         </Nav>
     );
 }

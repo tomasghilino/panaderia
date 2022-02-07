fe2c05e36fae4b68b6d36549a0ef2675cf146b2d
Franco Brumatti <franco.brumatti@gmail.com>
2022-02-07 17:57:45 -0300

    texto menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 15rem;
+    height: 17rem;
 
     background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
       url(${(props) => props.background}) no-repeat fixed center;
@@ -24,6 +24,35 @@ const Menu = () => {
     align-items: center;
     padding: 5rem;
     font-size: 1.2rem;
+  `;
+
+  const TextoMenuLista = styled.div`
+    display: block;
+    text-align: center;
+    padding: 4rem;
+    
+    ul {
+      color: #252525;
+      font-weight: bold;
+      list-style: none;
+    }
+
+    li::before {
+      content: "•";
+      font-size: 130%;
+      line-height: 0;
+      margin: 0 0.3rem 0 -0.25rem;
+      position: relative;
+      top: 0.08rem;
+      color: red;
+    }
+
+    @media(min-width: 1024px) {
+      display: flex;
+      justify-content: center;
+      text-align: start;
+      gap: 3rem;
+    }
   `;
 
   return (
@@ -46,25 +75,65 @@ const Menu = () => {
         </p>
       </TextoMenu>
       <ImagenTitulo background={`/media/menulunch.jpg`}>
-        <h1>Catering Artesanal</h1>
-        <p>- Nuestro menu -</p>
+        <h1>Lunch</h1>
       </ImagenTitulo>
-      <TextoMenu>
-        <p>
-          Ofrecemos el servicio para pequeños eventos familiares y
-          empresariales, que el cliente mismo viene a retirar o se lo llevamos a
-          su domicilio.
-        </p>
-        <p>
-          Los pedidos de catering pueden abarcar todos los productos que
-          hacemos, en ese sentido, nosotros lo asesoramos en las combinaciones
-          de sabores y cantidades de porciones más adecuadas para cada tipo de
-          reunión.
-        </p>
-      </TextoMenu>
+      <TextoMenuLista>
+        <div>
+          <ul>
+            <li>Chips</li>
+            <li>Fosforito</li>
+            <li>Saladitos</li>
+            <li>Saborizados con pavita y tomate</li>
+            <li>Figacitas con matambre casero</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Bruschettas</li>
+            <li>Pletzalej</li>
+            <li>Tortitas negras con jamon crudo</li>
+            <li>Locatellis</li>
+            <li>Pizzetas</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Empanadas</li>
+            <li>Calentitos</li>
+            <li>Brochets</li>
+            <li>Otros...</li>
+          </ul>
+        </div>
+      </TextoMenuLista>
       <ImagenTitulo background={`/media/menupasteleriadulce.jpg`}>
-        <h1>Catering Artesanal</h1>
-        <p>- Nuestro menu -</p>
+        <h1>Pasteleria Dulce</h1>
+      </ImagenTitulo>
+      <TextoMenuLista>
+      <div>
+          <ul>
+            <li>Masas finas</li>
+            <li>Masas secas</li>
+            <li>Patit fours</li>
+            <li>Monoporciones</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Tartas</li>
+            <li>Postres</li>
+            <li>Macarrones</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Tortas</li>
+            <li>Tortas temáticas</li>
+            <li>Otros...</li>
+          </ul>
+        </div>
+      </TextoMenuLista>
+      <ImagenTitulo background={`/media/menusandwich.jpg`}>
+        <h1>Sandwich</h1>
       </ImagenTitulo>
       <TextoMenu>
         <p>
